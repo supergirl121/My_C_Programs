6d48aedebbe9d2f76403e7461c889c9746c61484
Anitha Reddy <62069919+supergirl121@users.noreply.github.com>
2023-09-22 20:26:01 +0530

    Add files via upload

---
+++ Variable.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+    int main()
+    {
+        /*here the variable is number*/
+        int number = 25;
+
+        char star = '*';
+
+        int age = 16;
+        /*we can update a variable*/
+        age = 17;
+
+        float pi = 3.14;
+
+        int a = 40;
+        int A = 30;
+        int _age = 16;
+        int final_score = 10;
+
+        //using \n command for printing code in new line
+
+        printf("%d\n", age);
+        printf("%d",a);
+
+        //testing the single lined comments
+
+        return 0;
+    }
