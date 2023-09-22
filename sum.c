b5fc74ad89fe96335388807728b7a0ffe1c6dd69
Anitha Reddy <62069919+supergirl121@users.noreply.github.com>
2023-09-22 20:24:50 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+    int main()
+    {
+        int a, b;
+        printf("enter a\n");
+        scanf("%d", &a);
+
+        printf("enter b\n");
+        scanf("%d", &b);
+
+        int sum = a + b;
+        printf("sum is %d", sum);
+        return 0;
+    }
