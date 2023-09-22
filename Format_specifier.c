a581b9a16c1748d65230b21b326384f4036a8d8b
Anitha Reddy <62069919+supergirl121@users.noreply.github.com>
2023-09-22 20:25:41 +0530

    Add files via upload

---
+++ Format_specifier.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+    int main()
+    {
+        float pi = 3.14;
+        printf("age is %f \n", pi);
+        char star = '*';
+        printf("star is %c\n", star);
+        return 0;
+    }
