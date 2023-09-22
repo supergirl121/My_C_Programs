880a608b585d3a4379cb70db5e2aab7502fdb410
Anitha Reddy <62069919+supergirl121@users.noreply.github.com>
2023-09-22 20:25:26 +0530

    Add files via upload

---
+++ Scan.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+    int main()
+    {
+        int age;
+        printf("enter your age\n");
+        scanf("%d", &age);
+        printf("age is %d", age);
+        return 0;
+    }
